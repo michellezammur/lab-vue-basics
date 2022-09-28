8e734e2751cea7e66cab9ce73a07f8e471a62381
Michelle Zammur <mzammur@gmail.com>
2022-09-28 12:36:54 +0200

    lab-vue-basics

---
+++ lab-vue-basics/src/components/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- // Interaction 2 -->
+
+    <div class="lab">
+        <h1>{{stringLab()}}</h1>
+    </div>
+    <br />
+    <br />
+
+    <!-- Interaction 4 -->
+
+    <div v-for="(post, index) in posts" :key="index">
+        <ul>
+         <li>
+           <h3>{{post.title}}</h3>
+           <p>{{post.description}}</p>
+          <h5>{{post.content}}</h5>
+         </li>
+        </ul>
+    </div>
+
+</template>
+
+<script setup>
+import { ref , computed} from "vue";
+
+// <!-- // Interaction 2 -->
+
+const stringLab = () => {
+    return "String";
+};
+
+
+// <!-- Interaction 4 -->
+
+const posts = ref([
+    {
+      title: "Ursus maritimus",
+      description: "El oso polar u oso blanco (Ursus maritimus) es una especie de mamífero carnívoro.",
+        content: "El oso polar presenta un perfil más alargado que el de otros osos y las patas más desarrolladas, tanto para caminar como para nadar largas distancias.  ",
+    },
+
+    {
+      title: "Rangifer tarandus",
+      description: "El reno, es lo mismo que el ciervo (subespecies euroasiáticas) o caribú (subespecies americanas) (Rangifer tarandus).",
+      content:"Los renos o caribúes se encuentran en su rango natural en el norte de Fenoscandia, en Rusia, Groenlandia, Canadá y Alaska. ",
+    },
+
+    {
+      title: "Spheniscidae",
+      description: "Los pingüinos (Spheniscidae) son una familia de aves, la única del orden Sphenisciformes.",
+      content:"Los pingüinos son las únicas aves vivientes no voladoras adaptadas al buceo propulsado por las alas.",
+    },
+    
+]);
+
+
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ lab-vue-basics/src/components/NavBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav>
+
+    <ul>
+        <li href="">Link 1</li>
+        <li href="">Link 2</li>
+    </ul>
+  </nav>
+
+  <!-- Interaction 3 -->
+  <div>
+      
+      <h2 v-show="interactionThree">La condición es verdadera</h2>
+      
+    </div>
+
+    <br />
+    <br />
+    <!-- Interaction 5 -->
+<div class="container" :class="changeColor ? 'is-red' : 'is-orange' ">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi tempora quis assumenda maxime suscipit, cupiditate numquam dolorem placeat corrupti officia aliquid modi veniam molestias est? Aperiam sed amet eveniet ratione.</p>
+
+    <button @click="changeColorFunction">Click Me to toogle color!</button>
+
+</div>
+
+
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// Interaction 3
+const interactionThree = ref(true); 
+
+
+// Interaction 5
+
+const changeColor = ref(true);
+function changeColorFunction() {
+    changeColor.value = !changeColor.value;
+}
+
+</script>
+
+<style scoped>
+
+.container{
+    min-height: 20 rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.is-red{
+    background-color: red;
+}
+
+.is-orange{
+    background-color: orange;
+}
+
+p{
+    color:black;
+    text-align: center;
+}
+
+
+</style>
